60bce1a3cac816dd8e02bc49d62467e597a20671
fermion <htmlcss@yyt.yt>
2024-12-04 02:41:32 +0100

    Added C++ source code

---
+++ sports_record_center.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello!\n"
+         << "1. Football\n"
+         << "2. Basketball\n"
+         << "3. American Football\n"
+         << "4. Baseball\n"
+         << "5. Exit" << endl;
+
+    int option = 0;
+    do
+    {
+        cout << "Enter an option> ";
+        cin >> option;
+        switch (option)
+        {
+        case 1:
+            cout << "Football - N/A\n";
+            break;
+        case 2:
+            cout << "Basketball - N/A\n";
+            break;
+        case 3:
+            cout << "NFL - N/A\n";
+            break;
+        case 4:
+            cout << "Baseball - N/A\n";
+            break;
+        default:
+            break;
+        }
+    } while (option != 5);
+
+    return 0;
+}
